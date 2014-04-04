12588fe7cce4d927143fc1558ea4b2b77f052036
Jonathan Ballands <jballands@gmail.com>
2014-04-04 19:06:56 -0400

    Added some routing and started the .create route

--- js/app.js
+++ js/app.js
@@ -1,5 +1,82 @@
 /*
- *  Defines the app for Angular.js
+ *  Defines the ShopMaster app using Angular.js
+ *
+ *  Coded with <3 by Jonathan Ballands :)
  */
 
-var shopMaster = angular.module("shopMaster", []);
+var shopMaster = angular.module("shopMaster", ["ngRoute"]);
+var categories = ["Pastry", "Produce", "Jam"];
+
+/* 
+ * -------
+ * Routing
+ * -------
+ */
+
+shopMaster.config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "templates/landing.html",
+        controller: "LandingCtrl"
+    }),
+    
+    $routeProvider.when("/create", {
+        templateUrl: "templates/create.html",
+        controller: "CreateCtrl"
+    });
+});
+
+
+/*
+ *  ----------
+ *  Directives
+ *  ----------
+ */
+
+
+/* 
+ * -----------
+ * Controllers
+ * -----------
+ */
+
+shopMaster.controller("LandingCtrl", function($scope, $location, $http) {
+    
+    // Verify email
+    $scope.verifyEmail = function() {
+        
+        // DEBUG
+        $location.path("/create");
+        
+        /*
+        var emailAddress = $scope.emailAddress;
+        
+        // Fire AJAX request
+        $http({method: "GET", 
+               url: "http://shopmasterbackend-env-pjjgskpgzv.elasticbeanstalk.com/emailVerify/" + emailAddress}).
+            
+            success(function(data, status, headers, config) {
+                // Handle success
+                $("#sm-loading-landing").css({"visibility": "invisible"});
+                
+                // Route to create
+                $location.path("templates/create.html");
+            }).
+            error(function(data, status, headers, config) {
+                // Handle error
+                $("#sm-loading-landing").css({"visibility": "invisible"});
+                
+                // TODO: Show that there was an error...
+            });
+        
+        // Show loading animation
+        $("#sm-loading-landing").css({"visibility": "visible"});
+        */
+    };
+
+});
+
+shopMaster.controller("CreateCtrl", function($scope, $location, $http) {
+    
+    // TODO: Something...
+
+});
